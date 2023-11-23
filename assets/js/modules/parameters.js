134250e089806093f375b434f273ab1e46ea5b67
nocared <paul.richarddu30@gmail.com>
2023-11-23 11:27:48 +0100

    MODIF - Structure modules

---
+++ assets/js/modules/parameters.js
@@ -0,0 +1,4 @@
+let sizeRow = 6;
+let sizeColumn = 7;
+
+export {sizeRow,sizeColumn};
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,9 @@
-
-let sizeRow = 6;
-let sizeColumn = 7;
-
-function init()
-{
-    const gridDiv = document.getElementById("grid");
-    // Calculer la taille de la grille en pixels
-    const gridSize = Math.min(window.innerWidth, window.innerHeight) * 0.8; // 80% de la taille de la fenêtre
-    gridDiv.style.width = gridSize + "px";
-    gridDiv.style.height = gridSize + "px";
-
-    // Calculer la taille des cases en fonction de la grille
-    const cellSize = gridSize / Math.max(sizeColumn, sizeRow);
-
-    gridDiv.style.gridTemplateColumns = 'repeat(' + sizeColumn + ', ' + cellSize + 'px)';
-    gridDiv.style.gridTemplateRows = 'repeat(' + sizeRow + ', ' + cellSize + 'px)';
-
-    for (let i = 0; i < sizeRow * sizeColumn; i++) {
-        let createDiv = document.createElement("div");
-        gridDiv.append(createDiv);
-        createDiv.classList.add("circle");
-    }
-
-    // REMETTRE A NONE
-    document.getElementById("overlayEnd").style.display = "none";
-}
+import { initDisplay } from "./modules/display.js";
 
 
 
-init();
+
+
+
+
+initDisplay();
